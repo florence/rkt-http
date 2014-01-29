aa2f44ef674240bb839256842543a8ef238d71a7
Asumu Takikawa <asumu@ccs.neu.edu>
2014-01-28 21:24:31 -0500

    Update package dependencies

--- info.rkt
+++ info.rkt
@@ -1,5 +1,6 @@
 #lang setup/infotab
 (define name "http")
 (define build-deps '("scribble-lib"))
-(define deps '())
+(define deps '("base" "typed-racket-lib" "typed-racket-more"
+               "rackunit-lib" "web-server-lib"))
 (define scribblings '(("main.scrbl" ())))
